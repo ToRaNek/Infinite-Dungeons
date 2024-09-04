3b0c12c6443663572232e7230b1ec2ee7e5493d8
Alexandre Dehaine <alexandredehaine@outlook.fr>
2024-09-04 11:36:14 +0200

    équilibrage mobs

--- src/main/java/Mob.java
+++ src/main/java/Mob.java
@@ -25,18 +25,18 @@ public class Mob extends Entity{
         MobType[] listMob = MobType.values();
         int longueur = listMob.length;
         int num = rdm.nextInt(longueur);
-        int hp =  (int)((80*rdm.nextInt(101)/100 + 20) *difficulty);
-        int defA =  (int)((5*rdm.nextInt(101)/100 + 10) *0.25*difficulty);
-        int defP =  (int)((5*rdm.nextInt(101)/100 + 10) *0.25*difficulty);
+        int hp =  (int)((80*rdm.nextInt(101)/100 + 20) *(0.05*difficulty));
+        int defA =  (int)((0*rdm.nextInt(101)/100 + 10) *0.03*difficulty);
+        int defP =  (int)((0*rdm.nextInt(101)/100 + 10) *0.03*difficulty);
         int dmgA;
         int dmgP;
         if(rdm.nextInt(2)==0){
-            dmgA =  (int)((5*rdm.nextInt(101)/100 + 5) *0.25*difficulty);
+            dmgA =  (int)((5*rdm.nextInt(101)/100 + 5) *0.08*difficulty);
             dmgP =  0;
 
         }else{
             dmgA =  0;
-            dmgP =  (int)((5*rdm.nextInt(101)/100 + 5) *0.25*difficulty);
+            dmgP =  (int)((5*rdm.nextInt(101)/100 + 5) *0.08*difficulty);
 
         }
         int gold =  (int)((10*rdm.nextInt(101)/100 + 5) *0.25*difficulty);
