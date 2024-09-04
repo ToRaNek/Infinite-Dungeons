fac2f9ccfbf45fc7295d2efed5610efe2bfe92df
Mathys Bollengier <mathys.bollengier.etu@univ-lille.fr>
2024-09-04 13:40:54 +0200

    Chengement visuel stats

--- src/main/java/Combat.java
+++ src/main/java/Combat.java
@@ -108,10 +108,10 @@ public class Combat implements Serializable {
         boolean resp = false;
         String rep;
         while(!resp){
-            System.out.println("\nVos Statistiques\t Statistiques du monstre\n Vite : "+ this.player.getSpeed()+ "🪶   Pv : "+ this.player.getHp() +"❤️\t Vite : "+ this.monster.getSpeed()+ "🪶   Pv : "+ this.monster.getHp()
-            +"❤️\n AttP : " + this.player.getDmgA()+ "⚔️  AttM : " + this.player.getDmgP()+"🪄\t AttP : " + this.monster.getDmgA()+ "⚔️  AttM : " + this.monster.getDmgP()
-            +"🪄\n ResP : " + this.player.getDefA()+ "🛡️  ResM : " + this.player.getDefP()+"⭐\t ResP : " + this.monster.getDefA()+ "🛡️  ResM : " + this.monster.getDefP()
-            +"⭐\n Or : " + this.player.getGold() + "🪙");
+            System.out.println("\nVos Statistiques\t Statistiques du monstre\n 🪶 : "+ this.player.getSpeed()+ "   ❤️ : "+ this.player.getHp() +"\t 🪶 : "+ this.monster.getSpeed()+ "   ❤️ : "+ this.monster.getHp()
+            +"\n ⚔️ : " + this.player.getDmgA()+ "  🪄 : " + this.player.getDmgP()+"\t ⚔️ : " + this.monster.getDmgA()+ "  🪄 : " + this.monster.getDmgP()
+            +"\n 🛡️ : " + this.player.getDefA()+ "  ⭐ : " + this.player.getDefP()+"\t 🛡️ : " + this.monster.getDefA()+ "  ⭐ : " + this.monster.getDefP()
+            +"\n 🪙 : " + this.player.getGold());
             System.out.println("\nQue voulez vous faire ?" + ENDLINE + "1 - Attaque        2 - Bloquer        3 - Changer Equipement       4 - Potions");
             rep = Utils.readString();
             if(rep.equals("1")){
