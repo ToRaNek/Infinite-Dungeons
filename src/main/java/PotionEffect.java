718e9b210f96848a962fced06a19445458b31931
Ambroise Donnet <ambroise.donnet.etu@univ-lille.fr>
2024-09-04 16:29:51 +0200

    reset la durée des effet

--- src/main/java/PotionEffect.java
+++ src/main/java/PotionEffect.java
@@ -11,12 +11,21 @@ public class PotionEffect<T extends Entity> {
 
     public PotionEffect(T entity) {
         this.entity = entity;
-        this.effectsStatut = new HashMap<>();
+        this.effectsStatut = new HashMap<Statut, Integer>();
     }
-
 
     public int getEffectDuration(Statut statut) {
         return this.effectsStatut.get(statut);
+    }
+
+    public void resetEffectDuration(Statut statut){
+        this.effectsStatut.put(statut, statut.getDuration());
+    }
+
+    public void downEffectDuration(Statut statut){
+        for (Statut this.effectsStatut.keySet() : statuts) {
+            
+        }
     }
 
 
