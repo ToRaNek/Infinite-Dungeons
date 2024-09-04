615d7b25982506eaf0248b0c26c3858e2670a655
Mathys Bollengier <mathys.bollengier.etu@univ-lille.fr>
2024-09-04 09:34:42 +0200

    Equilibrage des chances

--- src/main/java/EventsRandom.java
+++ src/main/java/EventsRandom.java
@@ -32,7 +32,7 @@ public class EventsRandom {
         + " Je suis ici pour vous servir." + ENDLINE 
         + " Je vous laisse regarder ma cargaison."
         + " Ici vous pouvoir voir mes différents objets :"
-        + " 1- " + shopList[1].toString() + "2- " + shopList[2].toString() + "3- " + shopList[3].toString());
+        + " 1 - " + shopList[1].toString() + "2- " + shopList[2].toString() + "3- " + shopList[3].toString());
     }
 
     public static int boss(int difficulty){
