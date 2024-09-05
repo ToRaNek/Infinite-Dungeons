b72e54aa24d5e5417435486c32c29c6bee525dde
Ambroise Donnet <ambroise.donnet.etu@univ-lille.fr>
2024-09-05 08:40:19 +0200

    Avancer sur les statuts (poison fait)

--- src/main/java/StatutEffect.java
+++ src/main/java/StatutEffect.java
@@ -1,19 +1,29 @@
 package main.java;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class StatutEffect<T extends Entity> {
+public class StatutEffect<T extends Entity> implements Serializable{
     
 
     private T entity;
     private Map<Statut, Integer> effectsStatut;
+    private int[] statChanges;
+    private ArrayList<Statut> listStatut;
 
     public StatutEffect(T entity) {
         this.entity = entity;
         this.effectsStatut = new HashMap<Statut, Integer>();
+        this.listStatut = new ArrayList<>();
+        this.statChanges = new int[Statut.values().length];
+        for (int i = 0; i < statChanges.length; i++){
+            statChanges[i] = 0;
+            listStatut.add(Statut.values()[i]);
+        }
+        
     }
 
     public int getEffectDuration(Statut statut) {
@@ -45,8 +55,18 @@ public class StatutEffect<T extends Entity> {
         // fonction qui servira à activer les effets
     }
 
-    private void poison(){
-        
+    private void poison(Entity caster){
+        int changement = statChanges[listStatut.indexOf(Statut.POISON)];
+        if(changement != 0){
+            this.entity.setDefA(entity.getDefA() + changement);
+            statChanges[listStatut.indexOf(Statut.POISON)] = 0;
+        }else{
+            this.effectsStatut.put(Statut.POISON, Statut.POISON.getDuration());
+        }
+        changement = (int)0.05*caster.getDmgP();
+        statChanges[listStatut.indexOf(Statut.POISON)] = changement;
+        int dmg = (int)0.1*caster.getDmgP();
+        this.entity.damageTaken(0, dmg, caster);
     }
 
 
